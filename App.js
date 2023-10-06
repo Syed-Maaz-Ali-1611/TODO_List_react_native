611bb8ca0b9318a6c0fb834797162316860af4bd
Syed-Maaz-Ali-1611 <maazali1611@gmail.com>
2023-10-06 16:15:57 +0500

    TODO commit

--- App.js
+++ App.js
@@ -1,75 +1,83 @@
 // React Native Counter Example using Hooks!
 
 import React , {useState} from 'react';
-import {StyleSheet, View, Text, KeyboardAvoidingView, Platform, TextInput, TouchableOpacity, Keyboard} from 'react-native';
+import {StyleSheet, View, Text, KeyboardAvoidingView, Platform, TextInput, TouchableOpacity, Keyboard,ScrollView} from 'react-native';
 import Task from './component/task';
 
 
 export default function App(){
   const [task, setTask]= useState();
-  const [taskItems, setTaskItems] = useState([]);
+  const [pendingTasks, setPendingTasks] = useState([]);
+  const [completedTasks, setCompletedTasks] = useState([]);
+  
+  
 
-  const handletask = () =>{
+  const handleTask = () => {
     Keyboard.dismiss();
-    // console.log(task);~
-    setTaskItems([...taskItems, task])
-    setTask(null);
-  }
-
+    setPendingTasks([...pendingTasks, task]);
+    setTask('');
+  };
 
   const completeTask = (index) => {
-    let itemsCopy = [...taskItems];
-    itemsCopy.splice(index, 1);
-    setTaskItems(itemsCopy);
-  }
+    const completedTask = pendingTasks[index];
+    setCompletedTasks([...completedTasks, completedTask]);
 
+    const updatedPendingTasks = [...pendingTasks];
+    updatedPendingTasks.splice(index, 1);
+    setPendingTasks(updatedPendingTasks);
+  };
+
+ 
   return (
+  
     <View style={styles.container}>
-      
-      
-      {/*TODO LIST TODAYS TASK*/} 
-
-      <View style= {styles.taskwrapper}>
-          <Text style={styles.textTitle}>Todays Tasks</Text>
-
-          <View style = {styles.items}>   
-          {
-            taskItems.map((items,index) =>{ 
-               return (
-                <TouchableOpacity key={index} onPress={() =>completeTask(index)}>
-                  <Task text={items} />
-                </TouchableOpacity>
-               ) 
-            })
-          }
+      <ScrollView>
+      <View style={styles.taskwrapper}>
+        <Text style={styles.textTitle}>Todays Tasks</Text>
+        <View style={styles.items}>
          
-
-          </View>
+   
+          {pendingTasks.map((item, index) => (
+            <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+              <Task text={item} />
+            </TouchableOpacity>
+          ))}
+          
+        </View>
       </View>
 
+      <View style={styles.taskwrapper}>
+        <Text style={styles.textTitle}>Completed Tasks</Text>
+        <View style={styles.items}>
+          {completedTasks.map((item, index) => (
+            <Task key={index} text={item} />
+          ))}
+        </View>
+      </View>
+      </ScrollView>
       <KeyboardAvoidingView
-       behavior={Platform.OS==="Andriod"?"padding":"height"}
-       style = {styles.writeTaskWrapper}>
-        <TextInput style= {styles.input}
-        placeholder='Write a task' 
-        onChangeText={text => setTask(text)}
-        value={task} />
-          <TouchableOpacity onPress={()=> handletask()}>
-            <View style = {styles.addWrapper}>
-              <Text style={styles.addText}>+</Text>
-              </View>
-              </TouchableOpacity>
-       </KeyboardAvoidingView>
+        behavior={Platform.OS === 'android' ? 'padding' : 'height'}
+        style={styles.writeTaskWrapper}>
+        <TextInput
+          style={styles.input}
+          placeholder="Write a task"
+          onChangeText={(text) => setTask(text)}
+          value={task}
+        />
+        <TouchableOpacity onPress={() => handleTask()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
-
-    
   );
-  }
+}
 // React Native Styles
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#dff0ea",
+    backgroundColor: "#8D8FAD",
   },
 
   taskwrapper:{
@@ -80,9 +88,9 @@ const styles = StyleSheet.create({
 
   textTitle:{
     fontSize: 30,
-    color : 'black',
+    color : 'white',
     fontWeight: '900',
-    fontFamily: 'dela',
+    fontFamily: 'DelaGothicOne-Regular',
   },
 
   items :{
